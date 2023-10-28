92e1b0c1ca4f2ab0ef4292ee611d9fac914a7122
HASEEB <93050528+Haseeb2949J@users.noreply.github.com>
2023-10-28 12:23:55 +0530

    Create gcd.cpp

---
+++ Selection Sort/gcd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int gcd(int a,int b){
+    if(b==0){
+        return a;
+    }
+    return gcd(b,a%b);
+}
+int main(){
+    int a,b;
+    cout<<"Enter the value of a and b ";
+    cin>>a>>b;
+    cout<<"GCD = "<<gcd(a,b)<<endl;
+}
